07d98614cd025519d010bf30eef2d17a6c7c5433
Solomon Tesfaye <solomon.tesfaye@ocsbstudent.ca>
2021-03-25 16:24:26 +0000

    jv

--- script.js
+++ script.js
@@ -5,13 +5,14 @@ document.getElementById('enter').addEventListener('click', enter)
 
 function enter () {
   day = document.getElementById('input').value
-  if (day === 'Monday'||day === 'Thusday'||day === 'Wensday'|| day === 'Thursday'|| day === 'Friday') { alert('Time for school !') 
-  day = document,getElementById('input2')
-  .value 
-   } if (day === 'Saturday'||day === 'Sunday') { alert('Time to relax')
-   ('age').value
-   if (age >= 18 && day != 'Sunday' &&day != 'Saturday') 
-    document.getElementById('enter') 
-     alert('Time to work')
+  if (day === 'Monday' || day === 'Thusday'|| day === 'Wensday' || day === 'Thursday' || day === 'Friday')
+  { alert('Time for school!') 
+   day = document.getElementById('input2')
+.value 
+  } if (day === 'Saturday'|| day === 'Sunday') { alert('Time to relax')
+  ('age').value
+  if (age >= 18 && day != 'Sunday' &&day != 'Saturday') 
+   document.getElementById('enter') 
+  alert('Time for work')
  } 
 }
