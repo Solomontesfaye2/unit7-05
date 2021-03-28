507c1bc5bf07d4322c3790e003e19d0421834190
Solomon Tesfaye <solomon.tesfaye@ocsbstudent.ca>
2021-03-28 03:07:49 +0000

    enter

--- script.js
+++ script.js
@@ -10,12 +10,8 @@ function enter () {
      alert('Time for school!')
         day = document.getElementById('input2')
 .value
-  } 
-  if (day === 'Saturday' || day === 'Sunday') { innerHTML('Time to relax')
-   ('age').value }
-  age = document.getElementById('input2').value
   }
-  if (age >= 18 && day !== 'Sunday' && day !== 'Saturday') {
+  if (age >= 18 && day !== 'Sunday' && day !== 'Saturday') 
     document.getElementById('enter')
   alert('Time for work')
 }
