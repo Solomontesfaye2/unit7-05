99049b63906ba895d2b3e5087e1475453bc30312
Solomon Tesfaye <solomon.tesfaye@ocsbstudent.ca>
2021-03-28 03:57:11 +0000

    jv fixed

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-document.getElementById('enter').addEventListener('click', enter)
+let bass1 = 0
+let bass2 = 0
+let height = 0
+let answer = 0
 
-function enter () {
-  day = document.getElementById('input').value }
-  day = parseInt('day')
- if (day === 'Monday' || day === 'Thusday' || day === 'Wensday' || day === 'Thursday' || day === 'Friday')
+document.getElementById('calculate').addEventListener('click', calculate)
+
+function calculate () {
+  bass1 = document.getElementById('bass-1').value
+  bass1 = parseInt(bass1)
+  bass2 = document.getElementById('bass-2').value
+  bass2 = parseInt(bass2)
+  height = document.getElementById('height').value
+  height = parseInt(height)
+  answer = (bass1 + bass2) * height / 2
+  alert(answer)
+}
