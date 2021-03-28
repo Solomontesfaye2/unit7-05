ad1316d058da63430a017399855751fd5639fe4f
Solomon Tesfaye <solomon.tesfaye@ocsbstudent.ca>
2021-03-28 03:51:10 +0000

    vvvvv

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ document.getElementById('enter').addEventListener('click', enter)
 
 function enter () {
   day = document.getElementById('input').value }
+  day = parseInt('day')
  if (day === 'Monday' || day === 'Thusday' || day === 'Wensday' || day === 'Thursday' || day === 'Friday')
